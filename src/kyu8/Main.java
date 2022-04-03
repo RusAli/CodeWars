7ba571b1c8a964ea7be73f9f579e34ef956b5515
RusAli <mr.al1mov.ruslan@gmail.com>
2022-04-03 22:45:18 +0300

    3 ex done

--- src/kyu8/Main.java
+++ src/kyu8/Main.java
@@ -78,10 +78,188 @@ public class Main {
     return result;
   }
 
+//  5)
+
+//  Playing with cubes I
+//  https://www.codewars.com/kata/playing-with-cubes-i/train/java
+
+  /*
+  Create a public class called Cube without a constructor which gets one single private integer variable Side, a getter GetSide() and a setter SetSide(int num) method for this property. Actually, getter and setter methods are not the common way to write this code in C#. In the next kata of this series, we gonna refactor the code and make it a bit more professional...
+
+  Notes:
+
+  There's no need to check for negative values!
+  initialise the side to 0.
+*/
+
+  public class Cube{
+    private int Side;
+
+    public int getSide() {
+      return this.Side;
+    }
+
+    public void setSide(int side) {
+      this.Side = side;
+    }
+  }
+
+//  6)
+
+//  Building blocks
+//  https://www.codewars.com/kata/building-blocks/train/java
+/*
+  Write a class Block that creates a block (Duh..)
+
+          ##Requirements
+  The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+  Define these methods:
+          `getWidth()` return the width of the `Block`
+          `getLength()` return the length of the `Block`
+          `getHeight()` return the height of the `Block`
+          `getVolume()` return the volume of the `Block`
+          `getSurfaceArea()` return the surface area of the `Block`
+
+          ##Examples
+  Block b = new Block(new int[]{2,4,6}) -> creates a `Block` object with a width of `2` a length of `4` and a height of `6`
+          b.getWidth() // -> 2
+          b.getLength() // -> 4
+          b.getHeight() // -> 6
+          b.getVolume() // -> 48
+          b.getSurfaceArea() // -> 88
+  */
+
+  public class Block{
+
+    int width;
+    int length;
+    int height;
+    int volume;
+    int surfaceArea;
+
+    public int getWidth() {
+      return width;
+    }
+
+    public int getLength() {
+      return length;
+    }
+
+    public int getHeight() {
+      return height;
+    }
+
+    public int getVolume() {
+      return volume;
+    }
+
+    public int getSurfaceArea() {
+      return surfaceArea;
+    }
+
+
+    public Block(int[] block){
+      width = block[0];
+      length = block[1];
+      height = block[2];
+
+      volume = width*length*height;
+      surfaceArea = 2 * (width*length + width*height + length*height);
+
+    }
+  }
+
+//  7)
+//  Two fighters, one winner.
+//  https://www.codewars.com/kata/577bd8d4ae2807c64b00045b/train/java
+
+  /*
+  Create a function that returns the name of the winner in a fight between two fighters.
+  Each fighter takes turns attacking the other and whoever kills the other first is victorious.
+  Death is defined as having health <= 0.
+
+  Each fighter will be a Fighter object/instance.
+  See the Fighter class below in your chosen language.
+
+  Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0.
+  You can mutate the Fighter objects.
+
+  Example:
+  declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+
+  Lew attacks Harry; Harry now has 3 health.
+  Harry attacks Lew; Lew now has 6 health.
+  Lew attacks Harry; Harry now has 1 health.
+  Harry attacks Lew; Lew now has 2 health.
+  Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+
+  public class Fighter {
+    public String name;
+    public int health, damagePerAttack;
+    public Fighter(String name, int health, int damagePerAttack) {
+      this.name = name;
+      this.health = health;
+      this.damagePerAttack = damagePerAttack;
+    }
+  }
+
+*/
+  public class Fighter {
+    public String name;
+    public int health, damagePerAttack;
+
+    public Fighter(String name, int health, int damagePerAttack) {
+      this.name = name;
+      this.health = health;
+      this.damagePerAttack = damagePerAttack;
+    }
+  }
+
+  public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+    if (!fighter1.name.equals(firstAttacker)) {
+      return declareWinner(fighter2, fighter1, firstAttacker);
+    }
+
+    fighter2.health -= fighter1.damagePerAttack;
+    if (fighter2.health <= 0) {
+      return firstAttacker;
+    }
+    return declareWinner(fighter2, fighter1, fighter2.name);
+  }
 
 
 
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
--- src/kyu8/Test.java
+++ src/kyu8/Test.java
@@ -2,22 +2,8 @@ package kyu8;
 
 public class Test {
 
-  public static String countingSheep(int num) {
-
-    String result = "";
-
-    for( int i = 1; i <= num; i++){
-      result += i + " sheep...";
-    }
-
-    return result;
-  }
-
-
 
   public static void main(String[] args) {
-
-    System.out.println(countingSheep(4));
 
 
 
