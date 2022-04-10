16402c91fcabb79b06c166c90cffd1c83ba066d3
RusAli <mr.al1mov.ruslan@gmail.com>
2022-04-11 00:11:48 +0300

    Task 8 completed

--- src/kyu8/Test.java
+++ src/kyu8/Test.java
@@ -2,9 +2,24 @@ package kyu8;
 
 public class Test {
 
+  public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+
+    int sum =0;
+    for(int i =0; i < classPoints.length; i++){
+      sum += classPoints[i];
+    }
+    int avg = sum/classPoints.length;
+    if(avg > yourPoints){
+      return false;
+    }else {
+      return true;
+    }
+  }
 
   public static void main(String[] args) {
 
+
+    System.out.println(betterThanAverage(new int[] {100, 40, 34, 57, 29, 72, 57, 88}, 75));
 
 
   }
