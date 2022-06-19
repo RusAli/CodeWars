ecb8539bf6eb237285f739d2ec29194d99533894
Ruslan Alimov <63790145+RusAli@users.noreply.github.com>
2022-06-19 23:26:17 +0300

    21) Return Negative (#3)

--- src/kyu8/Solutions.java
+++ src/kyu8/Solutions.java
@@ -504,8 +504,28 @@ Complete the method that takes a boolean value and return a "Yes" string for tru
     }
   }
 
+//  21)
+//  Return Negative
+//  https://www.codewars.com/kata/55685cd7ad70877c23000102/train/java
+/*
+  In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
+  Examples
+  Kata.makeNegative(1);  // return -1
+  Kata.makeNegative(-5); // return -5
+  Kata.makeNegative(0);  // return 0
+  Notes
+  The number can be negative already, in which case no change is required.
+  Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+*/
 
+  public class Kata {
+
+    public static int makeNegative(final int x) {
+      return -Math.abs(x);
+    }
+
+  }
 
 
 
