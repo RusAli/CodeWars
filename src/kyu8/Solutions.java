d5b2eea694ee373bfb2af5556036c8381b947845
RusAli <mr.al1mov.ruslan@gmail.com>
2022-06-19 23:03:08 +0300

    20) Sum of positive

--- src/kyu8/Solutions.java
+++ src/kyu8/Solutions.java
@@ -475,9 +475,34 @@ Complete the method that takes a boolean value and return a "Yes" string for tru
     }
   }
 
+//  20)
+//  Sum of positive
+//  https://www.codewars.com/kata/5715eaedb436cf5606000381/train/java
+/*
+  You get an array of numbers, return the sum of all of the positives ones.
 
+  Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
+  Note: if there is nothing to sum, the sum is default to 0.
+*/
 
+  public class Positive{
+
+    public static int sum(int[] arr){
+
+      if(arr != null && arr.length > 0){
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+          if(arr[i] > 0){
+            sum +=arr[i];
+          }
+        }
+        return sum;
+      }
+      return 0;
+    }
+  }
 
 
 
