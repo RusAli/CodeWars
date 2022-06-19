fcd808f8689b195f31a06aa1a1ac6476ba4848cd
RusAli <mr.al1mov.ruslan@gmail.com>
2022-06-19 22:54:06 +0300

    19) Even or Odd

--- src/kyu8/Solutions.java
+++ src/kyu8/Solutions.java
@@ -456,6 +456,25 @@ Complete the method that takes a boolean value and return a "Yes" string for tru
 
 }
 
+//  19)
+//  Even or Odd
+//  https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/java
+
+ /*
+  Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+  */
+  public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+      if(number % 2 == 0 ){
+        return "Even";
+      }else {
+        return "Odd";
+      }
+
+    }
+  }
+
 
 
 
