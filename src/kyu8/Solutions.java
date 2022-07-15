1bb013568b5551da9ee4d48a4c8281cd5aaaa5d6
Ruslan Alimov <63790145+RusAli@users.noreply.github.com>
2022-07-16 00:16:55 +0300

    Are You Playing Banjo? (#7)

--- src/kyu8/Solutions.java
+++ src/kyu8/Solutions.java
@@ -581,7 +581,30 @@ public class Solutions {
     }
   }
 
+  /**
+   * 25)
+   * Are You Playing Banjo?
+   * https://www.codewars.com/kata/53af2b8861023f1d88000832/train/java
+   *
+   * Create a function which answers the question "Are you playing banjo?".
+   * If your name starts with the letter "R" or lower case "r", you are playing banjo!
+   * The function takes a name as its only argument, and returns one of the following strings:
+   * name + " plays banjo"
+   * name + " does not play banjo"
+   *
+   * Names given are always valid strings.
+   */
 
+  public static String areYouPlayingBanjo(String name) {
+    if(name.toUpperCase().charAt(0) == 'R'){
+
+      return name + " plays banjo";
+    }
+
+    return name + " does not play banjo";
+  }
+
+  
 
 }
 
@@ -595,7 +618,7 @@ public class Solutions {
 
 
 
-}
+
 
 
 
