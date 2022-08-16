f1e62dd7e68ca3fb8dfa9bcf5b8fe2f83683289c
RusAli <mr.al1mov.ruslan@gmail.com>
2022-08-16 22:54:29 +0300

    26) String repeat

--- src/kyu8/Solutions.java
+++ src/kyu8/Solutions.java
@@ -604,7 +604,23 @@ public class Solutions {
     return name + " does not play banjo";
   }
 
-  
+  /**
+   * 26)
+   * String repeat
+   * https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/java
+   *
+   * Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+   *
+   * Examples (input -> output)
+   * 6, "I"     -> "IIIIII"
+   * 5, "Hello" -> "HelloHelloHelloHelloHello"
+   *
+   */
+
+  public static String repeatStr(final int repeat, final String string) {
+    return "";
+  }
+
 
 }
 
