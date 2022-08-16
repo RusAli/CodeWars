8cec0e236a292534f7d600203f6dfce09ed938f7
RusAli <mr.al1mov.ruslan@gmail.com>
2022-08-16 23:00:43 +0300

    27

--- src/kyu8/Solutions.java
+++ src/kyu8/Solutions.java
@@ -621,6 +621,17 @@ public class Solutions {
     return string.repeat(repeat);
   }
 
+  /**
+   * 27)
+   * Remove String Spaces
+   * https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/java
+   *
+   * Simple, remove the spaces from the string, then return the resultant string.
+   *
+   */
+  public static String noSpace(final String x) {
+    return x.replace(" ","");
+  }
 
 }
 
