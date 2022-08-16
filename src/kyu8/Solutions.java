125a587714ce18159dadf9d3176f1770a8f6c771
Ruslan Alimov <63790145+RusAli@users.noreply.github.com>
2022-08-16 23:39:59 +0300

    29) Century From Year

--- src/kyu8/Solutions.java
+++ src/kyu8/Solutions.java
@@ -658,6 +658,29 @@ public class Solutions {
     return sum;
   }
 
+  /**
+   * 29)
+   * Century From Year
+   * https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/java
+   *
+   * Introduction
+   * The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+   *
+   * Task
+   * Given a year, return the century it is in.
+   *
+   * Examples
+   * 1705 --> 18
+   * 1900 --> 19
+   * 1601 --> 17
+   * 2000 --> 20
+   */
+
+  public static int century(int number) {
+
+    return (int) Math.ceil((double) number / 100);
+  }
+
 }
 
 
