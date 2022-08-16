0a146da1f5433cd5e65882c205ea0c8474a7ea59
RusAli <mr.al1mov.ruslan@gmail.com>
2022-08-16 23:21:17 +0300

    clean code

--- src/kyu8/Test.java
+++ src/kyu8/Test.java
@@ -4,18 +4,6 @@ public class Test {
 
 
   public static void main(String[] args) {
-    System.out.println(summation(8));
-  }
-
-
-  public static int summation(int n) {
-
-    int sum = 0;
-    for (int i = 1; i <= n; i++) {
-      sum += i;
-    }
-
-    return sum;
 
   }
 
