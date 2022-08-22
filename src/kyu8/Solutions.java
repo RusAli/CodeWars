88f8b8c8a2aa0f0cfdb1b644c0299b94abdd5944
Ruslan Alimov <63790145+RusAli@users.noreply.github.com>
2022-08-22 22:42:46 +0300

    31) Keep Hydrated! (#14)

--- src/kyu8/Solutions.java
+++ src/kyu8/Solutions.java
@@ -699,6 +699,30 @@ public class Solutions {
     return (n%x ==0) && (n%y ==0);
   }
 
+  /**
+   * 31)
+   * Keep Hydrated!
+   * https://www.codewars.com/kata/582cb0224e56e068d800003c/train/java
+   *
+   * Nathan loves cycling.
+   *
+   * Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+   *
+   * You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+   *
+   * For example:
+   *
+   * time = 3 ----> litres = 1
+   *
+   * time = 6.7---> litres = 3
+   *
+   * time = 11.8--> litres = 5
+   */
+
+  public int Liters(double time)  {
+    return (int)(time*0.5);
+  }
+
 }
 
 
