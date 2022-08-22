6310dfec9d439d8460e6158f4f5725c6bde1bb02
RusAli <mr.al1mov.ruslan@gmail.com>
2022-08-22 23:10:58 +0300

    kyu7 2) Vowel Count

--- src/kyu7/Test.java
+++ src/kyu7/Test.java
@@ -2,12 +2,6 @@ package kyu7;
 
 public class Test {
 
-
-  public static boolean validatePin(String pin) {
-
-    return pin.matches("\\d{4} | \\d{6}");
-  }
-
   public static void main(String[] args) {
 
 
